2809016cd8493bfbb31ee73883b0a388a23aacae
Nassim Morouche <nassim.morouche1@gmail.com>
2025-05-30 01:10:19 +0200

    Refactored the code a little bit

--- InstaTube/Reusable Components/VideoPlayer.swift
+++ InstaTube/Reusable Components/VideoPlayer.swift
@@ -9,18 +9,18 @@ import SwiftUI
 import AVKit
 
 struct VideoPlayer: UIViewControllerRepresentable {
-    @Binding var avPlayer: AVPlayer?
+    @Binding var player: AVPlayer?
     
     func makeUIViewController(context: Context) ->  AVPlayerViewController {
         let controller = AVPlayerViewController()
-        controller.player = avPlayer
+        controller.player = player
         controller.videoGravity = .resizeAspectFill
         controller.showsPlaybackControls = false
         return controller
     }
     
     func updateUIViewController(_ uiViewController: AVPlayerViewController, context: Context) {
-        uiViewController.player = avPlayer
+        uiViewController.player = player
     }
     
 }
